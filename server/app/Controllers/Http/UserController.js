66488ef9a49a0c8d30633ee74da869acbd132595
Menna-elshahawy <menna.elshahawy95@gmail.com>
2021-01-25 16:03:06 +0200

    registration / login

---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const User = use('App/Models/User');
+
+class UserController {
+	async register({request}){
+
+		const {email, password} = request.all();
+		// console.log(email, password);
+		user = await User.create({
+			email, 
+			password, 
+			username: email
+		});
+		return this.login(...arguments);
+	}
+// auth for authentication
+	async login({request, auth}){
+		const {email, password} = request.all();
+		const token = await auth.attempt(email, password);
+
+		return token;
+
+	}
+
+
+
+}
+
+module.exports = UserController
